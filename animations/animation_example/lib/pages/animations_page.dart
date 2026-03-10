64274f1b166e6919882737d874c8413e73d4a3d6
Tom Maurer <tmaurer@nait.ca>
2026-03-10 11:47:41 -0600

    animations done

--- animations/animation_example/lib/pages/animations_page.dart
+++ animations/animation_example/lib/pages/animations_page.dart
@@ -16,6 +16,9 @@ class _AnimationsPageState extends State<AnimationsPage>
   // This will drive our animations and provide controll access (stop, start, repeat, etc.)
   // It goes from 0-1 linearly over some duration
   late final AnimationController _controller;
+  late final Animation _animation;
+
+  var _endVal = 6.0;
 
   @override
   void initState() {
@@ -26,7 +29,10 @@ class _AnimationsPageState extends State<AnimationsPage>
       vsync: this,
       duration: Duration(seconds: 3),
     );
-    _controller.forward();
+    _controller.repeat();
+
+    // drive my curved animation off of the controller
+    _animation = _controller.drive(CurveTween(curve: Curves.elasticIn));
   }
 
   @override
@@ -43,7 +49,7 @@ class _AnimationsPageState extends State<AnimationsPage>
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 16,
+          spacing: 100,
           children: [
             // Explicit animation using basic controller
             AnimatedBuilder(
@@ -56,12 +62,54 @@ class _AnimationsPageState extends State<AnimationsPage>
                 );
               },
             ),
+            // Tween doesn't need a controller/animation
+            // it needs a tween and a duration
+            TweenAnimationBuilder(
+              // By default these run once, to get it to run again change the end value
+              tween: Tween<double>(begin: 1, end: _endVal),
+              duration: Duration(seconds: 3),
+              // Value is effectively like controller.value
+              // It's the current value of the tween
+              builder: (context, value, widget) {
+                return Transform.scale(
+                  scale: value,
+                  child: Transform.rotate(
+                    angle: value * math.pi,
+                    child: Text('Scale and rotate'),
+                  ),
+                );
+              },
+              onEnd: () => setState(() {
+                _endVal = _endVal == 1 ? 6.0 : 1;
+              }),
+            ),
+            // Drive this off a curved animation
+            AnimatedBuilder(
+              // any time this controller changes, call the builder
+              animation: _animation,
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: Offset(0, -_animation.value * 200),
+                  child: Text('Rotating', style: TextStyle(fontSize: 30)),
+                );
+              },
+            ),
+            // Implicit animation
+            RotationTransition(
+              turns: _controller,
+              child: Text("Implicit rotation"),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          //NOOP
+          // Stopping and starting animations
+          if (_controller.isAnimating) {
+            _controller.stop();
+          } else {
+            _controller.repeat();
+          }
         },
       ),
     );
