9085cd8137da5d58261c1c62b089799889902502
Tom Maurer <tmaurer@nait.ca>
2026-03-17 10:54:39 -0600

    camera pt 2

--- device_features/camera_example/lib/main.dart
+++ device_features/camera_example/lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:camera_example/pages/camera_page.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  // Ensure widget tree is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Ask the device for the cameras that are available
+  var cameras = await availableCameras();
+
+  runApp(MainApp(cameras: cameras));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({required this.cameras, super.key});
+
+  // list of all cameras on the device, pick one to pass to the page
+  final List<CameraDescription> cameras;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    // Pick the first camera and pass it through to the camera page for preview and pictures
+    return MaterialApp(home: CameraPage(cameraDescription: cameras[0]));
   }
 }
--- device_features/camera_example/lib/pages/camera_page.dart
+++ device_features/camera_example/lib/pages/camera_page.dart
@@ -25,6 +25,25 @@ class _CameraPageState extends State<CameraPage> {
       widget.cameraDescription,
       ResolutionPreset.max,
     );
+
+    // intialize camera controller getting permissions as needed
+    _cameraController
+        .initialize()
+        .then((_) {
+          // Camera permission has been granted, no errors
+          // We call an empty set state to force a rebuild of the page
+          setState(() {
+            // If we wanted to we could set camera presets here
+          });
+        })
+        .catchError((e) {
+          // error happened check what it was
+          if (e is CameraException && e.code == 'CameraAccessDenied') {
+            // The user has said no to camera permissions, decide how to handle it
+            // you could re-ask, shut down app, show error UI, etc.
+            throw Exception('Camera Permission Denied');
+          }
+        });
   }
 
   @override
@@ -34,8 +53,46 @@ class _CameraPageState extends State<CameraPage> {
     super.dispose();
   }
 
+  Future<XFile?> _takePicture() async {
+    // make sure we aren't already taking a picture
+    if (_cameraController.value.isTakingPicture) return null;
+
+    // try to take the picture
+    try {
+      // by default taking a picture stores the file in your local cache folder
+      // We return that here and then later on could move it, rename it, etc.
+      // Or just leave it where it is and read from there
+      final file = await _cameraController.takePicture();
+      return file;
+    } catch (e) {
+      // handle issues here
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      // Contains the button to navigate to gallery page
+      appBar: AppBar(),
+      // Camera Preview, showing what the camera sees
+      body: Center(child: CameraPreview(_cameraController)),
+      // Take a picture button
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var file = await _takePicture();
+
+          // Once you've take a picture make sure it actually worked
+          // and that we're still mounted
+          if (file != null && mounted) {
+            // Here you could move the photo, rename it, show some feedback
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Picture saved to: ${file.path}')),
+            );
+          }
+        },
+        child: Icon(Icons.camera),
+      ),
+    );
   }
 }
